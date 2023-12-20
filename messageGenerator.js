93f13b2ffaee22a7603d36920b6832161e3bd159
Jonce223 <61184934+Jonce223@users.noreply.github.com>
2023-12-20 18:33:45 +0200

    Added the code and added some code to it

---
+++ messageGenerator.js
@@ -0,0 +1,14 @@
+//Creating different Strings for each individual message
+let message1 = "hello world!";
+let message2 = "How are you?";
+let message3 = "Message Generator!";
+let message4 = "Do You want to play the game?";
+let message5 = "What's your name?";
+
+//Creating array for all the messages
+const messages = [message1, message2, message3, message4, message5];
+
+for(let i = 0; i < messages.length; i++){
+    console.log("The Message is:"+messages[Math.floor(Math.random()*5)]);
+    break;
+}
